d426fed32139adcd54ad4ebac86051276010a164
Eden Politte <y-github@neonepiphany.com>
2016-02-01 02:35:42 -0800

    Add temp art for Splash, Loading, and Field Scenes. Field -> Hive -> Field navigation.

---
+++ Girls with Masks/Assets/Scripts/SceneController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+using System.Collections;
+
+public class SceneController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void LoadFieldScene()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void LoadHiveScene()
+    {
+        SceneManager.LoadScene(3);
+    }
+
+}
---
+++ Girls with Masks/Assets/Scripts/UIButton.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UIButton : MonoBehaviour
+{
+    public GameObject targetObject;
+    public string targetMessage;
+
+	// Use this for initialization
+	void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+
+    void OnMouseUp()
+    {
+        print("click");
+        if (targetObject != null)
+        {
+            print("click");
+            targetObject.SendMessage(targetMessage);
+        }
+    }
+}
